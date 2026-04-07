58fed12c2f3792c65f45ab451ea16c484c1bea20
chonkycat123 <24aiml101@bnmit.in>
2026-04-07 09:47:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def home():
   return render_template('poco.html', title="homepage")
 if __name__=='__main__':
-  app.run(host='127.0.0.1', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True)
