29dc9fd884f08bd29530e42cc432cddca4352172
chonkycat123 <24aiml101@bnmit.in>
2026-04-07 09:20:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def home():
-  return render_template('index.html', title="homepage")
+  return render_template('poco.html', title="homepage")
 if __name__=='__main__':
   app.run(host='127.0.0.1', port=808, debug=True)
